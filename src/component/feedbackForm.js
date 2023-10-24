091cf5eca40d87f26c01618da68db4ba7646a7ac
Ekansh <ekanshbhushan2k22@gmail.com>
2023-10-24 12:28:37 +0530

    inital commit

--- src/component/feedbackForm.js
+++ src/component/feedbackForm.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import StarRating from './starRating'
+import MyComponent from './option'
+import './feedbackForm.css'
+// const preSetFeedback = {
+//     list: [
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//         { title: 'lorem ipsum', flag: false},
+//     ]
+// }
+
+// function reducer(state,action){
+//     switch(action.type){
+//         case 'ON_SELECT':
+//             return {...state, ...action.payload}
+//         default:
+//             return state
+//     }
+// }
+
 
 function feedbackForm() {
 
-    const preSetSkills = [
-        "lorem ipsum",
-        "lorem ipsum",
-        "lorem ipsum",
-        "lorem ipsum",
-        "lorem ipsum",
-        "lorem ipsum",
-    ];
+    
+    // const[state,dispatch] = useReducer(reducer, initialState)
   return (
     <div className='form'>
         <div>
@@ -22,11 +39,51 @@ function feedbackForm() {
 
       <StarRating/>
         </div>
-        <div>
+        <div >
             What Went Wrong
         </div>
+    
+        <div className='myoptions'>
+            <MyComponent/>
+        </div>
+        <form>
+        <input
+                                style={{
+                                    border: "2px solid #108CFF",
+                                    outline: "none",
+                                    width: "100%",
+                                    height: "100%",
+                                    fontSize: "16px",
+                                    fontWeight: "500",
+                                    color: "black",
+                                    backgroundColor: "transparent",
+                                    padding: "10px",
+                                }}
+                            />
 
-
+<button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    if (e.target.previousSibling.value === "")
+                                        return;
+                                    
+                                }}
+                                style={{
+                                    backgroundColor: "#108CFF",
+                                    color: "white",
+                                    fontSize: "16px",
+                                    fontWeight: "500",
+                                    border: "none",
+                                    outline: "none",
+                                    padding: "10px",
+                                    borderRadius: "5px",
+                                    cursor: "pointer",
+                                    marginLeft: "10px",
+                                }}
+                            >
+                                Add
+                            </button>
+        </form>
     </div>
   )
 }
